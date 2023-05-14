08d632e2155ea8d492a61773b7831a3936900f99
arsyanmw <arsyanmw12@gmail.com>
2023-05-14 12:55:25 +0700

    add win tag

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,7 +16,17 @@ const HomePage = () => {
                 setMatches(snapshot.val());
             }
         })
-    }, [])
+    }, []);
+
+    const WinTag = (players: Players[], winnerId: number) => {
+        return players?.map((player, idx) => {
+            if (winnerId === idx) {
+                return <p className='flex justify-center'><span className='win-tag px-1 rounded-full bg-lime-400 flex justify-center items-center'><FaCrown className='me-1'/> {player.name}</span> {idx === 0 ? <span className='mx-1'>VS</span> : ''}</p>;
+            } else {
+                return <p className='flex justify-center'><span className='win-tag flex justify-center'>{player.name}</span> {idx === 0 ? <span className='mx-1'>VS</span> : ''}</p>;
+            }
+        });
+    }
 
     return (
         <div className='container mx-auto p-4'>
@@ -26,10 +36,8 @@ const HomePage = () => {
                         return (
                             <Link to={`/match/${idx}`} key={idx}>
                                 <div className='card-list p-3 w-100 flex justify-between flex-row  rounded-lg shadow-md mb-5'>
-                                    <p>Match-{idx} <span className={'px-2 rounded-full text-xs ' + (match.status === 1 ? 'bg-lime-400' : 'bg-slate-400')}>{ match.status === 1 ? 'On Going' : 'Finished' }</span></p>
-                                    {match.players.length === 2 && (
-                                        <p>{match.players[0].name} VS {match.players[1].name}</p>
-                                    )}
+                                    <p>Match-{idx + 1} <span className={'px-2 rounded-full text-xs ' + (match.status === 1 ? 'bg-lime-400' : 'bg-slate-400')}>{ match.status === 1 ? 'On Going' : 'Finished' }</span></p>
+                                    <div className='flex justify-center'>{WinTag(match.players, match.winner)}</div>
                                 </div>
                             </Link>
                         )
@@ -38,6 +46,6 @@ const HomePage = () => {
             </div>
         </div>
     )
-}
+};
 
 export default HomePage;
