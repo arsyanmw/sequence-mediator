8d45837a5c3347bf9930d383d2e0cceae63de4b5
arsyanmw <arsyanmw12@gmail.com>
2023-06-01 03:13:35 +0700

    fix: iterate vs text on wintag

--- src/components/WinTag/index.tsx
+++ src/components/WinTag/index.tsx
@@ -5,9 +5,9 @@ import React from "react";
 export const WinTag = (players: Players[], winnerId: number | string) => {
     return players?.map((player, idx) => {
         if (winnerId === idx) {
-            return <p className='flex justify-center' key={idx}><span className='win-tag px-2 rounded-full bg-lime-400 flex justify-center items-center'><FaCrown className='me-1'/> {player.name}</span> {idx === 0 ? <span className='mx-1'>VS</span> : ''}</p>;
+            return <p className='flex justify-center' key={idx}><span className='win-tag px-2 rounded-full bg-lime-400 flex justify-center items-center'><FaCrown className='me-1'/> {player.name}</span> {idx !== players.length - 1 ? <span className='mx-1 italic font-semibold'>VS</span> : ''}</p>;
         } else {
-            return <p className='flex justify-center' key={idx}><span className='win-tag flex justify-center'>{player.name}</span> {idx === 0 ? <span className='mx-1'>VS</span> : ''}</p>;
+            return <p className='flex justify-center' key={idx}><span className='win-tag flex justify-center'>{player.name}</span> {idx !== players.length - 1 ? <span className='mx-1 italic font-semibold'>VS</span> : ''}</p>;
         }
     });
 }
